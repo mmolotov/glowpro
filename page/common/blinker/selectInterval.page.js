5d597a97cde411f2dbae3929b4cb12a1e65837f3
Maksim Gavrilov <maksim.gavrilov@partner.netconomy.net>
2025-01-25 11:53:57 +0100

    blinker flow: replace slider with picker

--- page/common/blinker/selectInterval.page.js
+++ page/common/blinker/selectInterval.page.js
@@ -1,14 +1,14 @@
-import {push} from '@zos/router'
-import {createWidget, event, prop, widget} from '@zos/ui'
+import {createWidget, widget} from '@zos/ui'
 import {LocalStorage} from '@zos/storage'
-import {calculateSliderValue, CONTROLS} from './selectInterval.page.layout.js';
-import {BUTTONS, COMMON, PAGES, TRANSLATION_KEYS} from '../layout/common.layout';
+import {push} from '@zos/router'
+import {CONTROLS, pickerIntervals, pickerValueMax} from './selectInterval.page.layout.js';
+import {COMMON, PAGES} from '../layout/common.layout';
 
 const selectedInterval = 'blinker.selectedInterval'
-const selectedIntervalY = 'blinker.selectedIntervalY'
 const storage = new LocalStorage()
 let colors = []
-let interval = 1000
+let intervals = pickerIntervals
+let interval = pickerValueMax
 
 Page({
     onInit(params) {
@@ -22,32 +22,18 @@ Page({
     },
     build() {
         const controlsContainer = createWidget(widget.VIEW_CONTAINER, COMMON.fullScreenContainer())
-        let header = controlsContainer.createWidget(widget.TEXT, COMMON.header(TRANSLATION_KEYS.selectInterval))
-        //todo: replace slider with picker
-        let sliderBar = controlsContainer.createWidget(widget.FILL_RECT, CONTROLS.sliderBar)
-        let sliderControl = controlsContainer.createWidget(widget.CIRCLE, CONTROLS.sliderControl(storage.getItem(selectedIntervalY)))
-        let sliderHover = controlsContainer.createWidget(widget.FILL_RECT, CONTROLS.sliderHover())
-        let sliderText = controlsContainer.createWidget(widget.TEXT, CONTROLS.sliderText(interval))
-        sliderHover.addEventListener(event.CLICK_UP, (info) => {
-            sliderBarSetValue(sliderControl, info)
-        })
-        sliderHover.addEventListener(event.MOVE, (info) => {
-            sliderBarSetValue(sliderControl, info)
-        })
-
-        const startButton = controlsContainer.createWidget(widget.BUTTON, BUTTONS.startButton(btn => {
-            push({
-                url:    PAGES.blinker,
-                params: {colors: colors, interval: interval}
-            })
-        }))
-
-        function sliderBarSetValue(sliderControl, info) {
-            sliderControl.setProperty(prop.MORE, CONTROLS.sliderSetValue(info))
-            sliderText.setProperty(prop.MORE, CONTROLS.sliderTextSetNewValue(info))
-            interval = calculateSliderValue(info)
-            storage.setItem(selectedInterval, interval)
-            storage.setItem(selectedIntervalY, info.y)
-        }
+        const picker = controlsContainer.createWidget(widget.WIDGET_PICKER,
+                CONTROLS.picker(interval, (picker, event_type, column_index, select_index) => {
+                    if (event_type === 1) {
+                        interval = intervals[select_index]
+                        storage.setItem(selectedInterval, interval)
+                    }
+                    if (event_type === 2) {
+                        push({
+                            url:    PAGES.blinker,
+                            params: {colors: colors, interval: interval}
+                        })
+                    }
+                }))
     }
 });
--- page/common/blinker/selectInterval.page.layout.js
+++ page/common/blinker/selectInterval.page.layout.js
@@ -1,101 +1,33 @@
 import {getText} from '@zos/i18n';
-import {align, text_style} from '@zos/ui'
 
-import {COLORS, DEVICE_INFO, TRANSLATION_KEYS} from '../layout/common.layout'
+import {TRANSLATION_KEYS} from '../layout/common.layout'
 
 //slider
-const sliderValueMin = 100
-const sliderValueMax = 1_000
-const sliderValueStep = 50
-// slider bar
-const yCoefficient = 0.2
-const xCoefficient = 0.6
-const hCoefficient = 0.55
-const sliderBarLineWith = 4
-const sliderBarW = 100
-const sliderBarRadius = sliderBarW / 2
-const sliderBarH = DEVICE_INFO.DEVICE_HEIGHT * hCoefficient
-const sliderBarY = DEVICE_INFO.DEVICE_HEIGHT * yCoefficient
-const sliderBarX = DEVICE_INFO.DEVICE_WIDTH * xCoefficient //(DEVICE_INFO.DEVICE_WIDTH - sliderBarW - sliderBarLineWith)
-//slider control
-const sliderControlRadius = sliderBarRadius - sliderBarLineWith * 2
-const sliderControlMax = sliderBarY + sliderBarH - sliderBarRadius
-const sliderControlMin = sliderBarY + sliderBarRadius
-const sliderControlX = sliderBarX + sliderBarRadius
-//slider text
-const textSize = 28
-const textH = textSize
-const textW = textH * 5
-const textXCoefficient = 0.2
-const textYCoefficient = 0.5
-const textX = DEVICE_INFO.DEVICE_WIDTH * textXCoefficient
-const textY = DEVICE_INFO.DEVICE_HEIGHT * textYCoefficient - textH / 2
+const pickerValueMin = 100
+const pickerValueMax = 1_000
+const pickerValueStep = 50
+
+const pickerIntervals = []
+for (let i = pickerValueMin; i <= pickerValueMax; i += pickerValueStep) {
+    pickerIntervals.push(i)
+}
 
 const CONTROLS = {
-    sliderControl(valueY = 0) {
+    picker(previousValue, callback) {
         return {
-            center_x: sliderControlX,
-            center_y: Math.max(sliderControlMin, Math.min(sliderControlMax, valueY)),
-            radius:   sliderControlRadius,
-            color:    COLORS.TEXT.BUTTON
+            nb_of_columns: 1,
+            title:         getText(TRANSLATION_KEYS.selectInterval),
+            data_config: [
+                {
+                    data_array:     pickerIntervals,
+                    support_loop:   true,
+                    init_val_index: pickerIntervals.indexOf(previousValue),
+                    unit:           getText(TRANSLATION_KEYS.milis)
+                }
+            ],
+            picker_cb:   callback
         }
-    },
-    sliderBar: {
-        x:          sliderBarX,
-        y:          sliderBarY,
-        w:          sliderBarW,
-        h:          sliderBarH,
-        radius:     sliderBarRadius,// The rectangle's rounded corners.
-        line_width: sliderBarLineWith,
-        color:      COLORS.SYS.BUTTON_HIGHLIGHT
-    },
-    sliderHover() {
-        let props = CONTROLS.sliderBar
-        props.alpha = 0
-        return props
-    },
-    sliderSetValue(info = {x: 0, y: 0}) {
-        return {
-            center_x: calculateSliderX(info),
-            center_y: calculateSliderY(info),
-            radius:   sliderControlRadius,
-            color:    COLORS.TEXT.BUTTON
-        }
-    },
-    sliderText(value) {
-        return {
-            x:          textX,
-            y:          textY,
-            w:          textW,
-            h:          textH,
-            color:      COLORS.TEXT.TITLE,
-            text_size:  textSize,
-            align_h:    align.RIGHT,
-            align_v:    align.CENTER_V,
-            text_style: text_style.NONE,
-            text:       Math.min(sliderValueMax, Math.max(sliderValueMin, value)) + ' ' + getText(TRANSLATION_KEYS.milis)
-        }
-    },
-    sliderTextSetNewValue(info = {x: 0, y: 0}) {
-        let props = this.sliderText()
-        props.text = calculateSliderValue(info) + ' ' + getText(TRANSLATION_KEYS.milis)
-        return props
     }
 }
 
-function calculateSliderX(info = {x: 0, y: 0}) {
-    return sliderControlX
-}
-
-function calculateSliderY(info = {x: 0, y: 0}) {
-    const result = Math.min(sliderControlMax, Math.max(sliderControlMin, info.y))
-    return result
-}
-
-function calculateSliderValue(info = {x: 0, y: 0}) {
-    const valueShifted = calculateSliderY(info) - sliderControlMax
-    const valueInPercents = Math.abs(parseInt(valueShifted / (sliderControlMax - sliderControlMin) * 100))
-    return sliderValueMin + Math.round((valueInPercents / 100) * (sliderValueMax - sliderValueMin) / sliderValueStep) * sliderValueStep
-}
-
-export {CONTROLS, calculateSliderValue}
+export {CONTROLS, pickerIntervals, pickerValueMax}
